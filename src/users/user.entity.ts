125b8bf50444a07b49523cf57f7752326caf9f97
ownfrezzy <ownfrezzy@gmail.com>
2022-09-20 15:45:37 +0300

    added create report route

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,18 +1,17 @@
-import {
-  AfterInsert,
-  AfterRemove,
-  AfterUpdate,
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 }
