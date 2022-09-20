47bc1f711bf2c05e206f3320e64d89c7168c72f2
ownfrezzy <ownfrezzy@gmail.com>
2022-09-20 03:15:52 +0300

    removed unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-import { DataSource } from 'typeorm';
 const cookieSession = require('cookie-session');
 
 @Module({
