fe273aee049baa5cfac4ab8cf2b8d6add5d289e6
ownfrezzy <ownfrezzy@gmail.com>
2022-09-20 03:14:51 +0300

    added e2e tests

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -15,19 +15,4 @@ export class User {
   email: string;
   @Column()
   password: string;
-
-  @AfterInsert()
-  logInsert() {
-    console.log('Inserted User with id', this.id);
-  }
-
-  @AfterUpdate()
-  logUpdate() {
-    console.log('Updated user with id', this.id);
-  }
-
-  @AfterRemove()
-  logRemove() {
-    console.log('Removed user with id', this.id);
-  }
 }
